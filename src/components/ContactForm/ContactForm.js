ec43747a73cc5a6990c82d37ee2488b8692907a3
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2022-12-28 22:41:34 +0200

    bug fix

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,73 +1,150 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import css from './ContactForm.module.css';
 import { nanoid } from 'nanoid';
 
-class ContactForm extends Component {
-  state = {
-    id: '',
-    name: '',
-    number: '',
+export function ContactForm({ onSubmit }) {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const setKey = e => {
+    // const addId = nanoid();
+    setId(nanoid()); //add unic ID
   };
 
-  setId = () => {
-    const addId = nanoid();//add unic ID
-    this.setState({ id: addId });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.resetForm();
+    onSubmit({ id, name, number });
+    resetForm();
   };
 
-  handleCange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+  const handleChangeName = e => {
+    const { value } = e.currentTarget;
+    setName(value);
   };
 
-  resetForm = () => {
-    this.setState({ name: '', number: '', id: '' });
+  const handleChangeNumber = e => {
+    const { value } = e.currentTarget;
+    setNumber(value);
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <br />
-          <input
-            id={this.addId}
-            className={css.nameInput}
+  const resetForm = () => {
+    setId('');
+    setName('');
+    setNumber('');
+  };
 
-            type="text"
-            name="name"
-            value={this.state.name}
-            onChange={this.handleCange}
-            onBlur={this.setId}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
         <br />
-        <label>
-          Number <br />
-          <input
+        <input
+          className={css.nameInput}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChangeName}
+          onBlur={setKey}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Number <br />
+        <input
           className={css.numberInput}
-            type="tel"
-            name="number"
-            value={this.state.number}
-            onChange={this.handleCange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <br />
-        <button className={css.btnAddContact} type="submit">Add contact</button>
-      </form>
-    );
-  }
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChangeNumber}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <br />
+      <button className={css.btnAddContact} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
 
 export default ContactForm;
+
+// import React, { Component } from 'react';
+// import css from './ContactForm.module.css';
+// import { nanoid } from 'nanoid';
+
+// class ContactForm extends Component {
+//   state = {
+//     id: '',
+//     name: '',
+//     number: '',
+//   };
+
+//   setId = () => {
+//     const addId = nanoid();//add unic ID
+//     this.setState({ id: addId });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.onSubmit(this.state);
+//     this.resetForm();
+//   };
+
+//   handleCange = e => {
+//     const { name, value } = e.currentTarget;
+//     this.setState({ [name]: value });
+//   };
+
+//   resetForm = () => {
+//     this.setState({ name: '', number: '', id: '' });
+//   };
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Name
+//           <br />
+//           <input
+//             id={this.addId}
+//             className={css.nameInput}
+
+//             type="text"
+//             name="name"
+//             value={this.state.name}
+//             onChange={this.handleCange}
+//             onBlur={this.setId}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//             required
+//           />
+//         </label>
+//         <br />
+//         <label>
+//           Number <br />
+//           <input
+//           className={css.numberInput}
+//             type="tel"
+//             name="number"
+//             value={this.state.number}
+//             onChange={this.handleCange}
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//             required
+//           />
+//         </label>
+//         <br />
+//         <button className={css.btnAddContact} type="submit">Add contact</button>
+//       </form>
+//     );
+//   }
+// }
+
+// export default ContactForm;
